dbd494097261f5e45f24ae3e12f4525d2bea6eb4
TBello <tbello@bellcomptech.com>
2022-08-10 16:27:11 -0400

    Changes to archive.sh

--- stages/04_archive.sh
+++ stages/04_archive.sh
@@ -7,9 +7,9 @@ registry=$(jq -r .archive.registry pipeline.json)
 repository=$(jq -r .archive.repository pipeline.json)
 image="${registry}/${repository}:latest"
 
-docker login \
+#docker login \
     -u "${DOCKER_USERNAME?:}" \
     -p "${DOCKER_PASSWORD?:}" \
     "${registry}"
-docker build -t "${image}" .
-docker push "${image}"
+#docker build -t "${image}" .
+#docker push "${image}"
