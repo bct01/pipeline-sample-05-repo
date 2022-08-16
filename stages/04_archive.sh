e81aa34363e06ae10ef7c8d0e3b57aa4de712251
TBello <tbello@bellcomptech.com>
2022-08-16 18:34:51 -0400

    Sample update 01

--- stages/04_archive.sh
+++ stages/04_archive.sh
@@ -5,7 +5,8 @@ echo "Archiving Application"
 
 registry=$(jq -r .archive.registry pipeline.json)
 repository=$(jq -r .archive.repository pipeline.json)
-image="${registry}/${repository}:latest"
+namespace=$(jq -r .archive.namespace pipeline.json)
+image="${registry}/${namespace}/${repository}:latest"
 
 docker login \
     -u "${DOCKER_USERNAME?:}" \
--- stages/05_deploy.sh
+++ stages/05_deploy.sh
@@ -6,6 +6,7 @@ echo "Deploying Application"
 container_port=$(jq -r .deploy.containerPort pipeline.json)
 registry=$(jq -r .archive.registry pipeline.json)
 repository=$(jq -r .archive.repository pipeline.json)
-image="${registry}/${repository}:latest"
+namespace=$(jq -r .archive.namespace pipeline.json)
+image="${registry}/${namespace}/${repository}:latest"
 
 docker run -dp "${container_port}:${container_port}" "${image}"
